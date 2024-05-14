1f5bf15b2c100bfb215eab25607f2b22d4f22261
Kovalik-Deák Bercel <150810553+bercikovalik@users.noreply.github.com>
2024-05-14 12:16:42 +0200

    Update main.py

--- SQL_python/main.py
+++ SQL_python/main.py
@@ -367,7 +367,7 @@ print('\n', "CRUD műveletek:", '\n')
 
 # ------------ Create ----------------
 
-"""
+
 db.Products.insert_one({
     "ProdutID": 120,
     "ProductName": "New High-End Speaker",
@@ -375,12 +375,7 @@ db.Products.insert_one({
     "Price": 450.00,
     "StockQuantity": 50
 })
-"""
-# ------------ Read ----------------
 
-results = db.Products.find({"Category": "Audio"})
-for product in results:
-    print(product)
 
 # ------------ Update ----------------
 db.Products.update_one(
@@ -391,12 +386,13 @@ db.Products.update_one(
 # ------------ Delete ----------------
 """
 query = {"ProductName": "New High-End Speaker"}
-
-
 result10 = db.Products.delete_many(query)
-
-
 print(f"Documents deleted: {result10.deleted_count}")
 """
+# ------------ Read ----------------
+
+results = db.Products.find({"Category": "Audio"})
+for product in results:
+    print(product)
 
 client.close()
